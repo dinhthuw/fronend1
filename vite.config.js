78f5e47951b8aa6d6c053c3d2bfd185b8d4fd71a
dinhthuw <90031105+dinhthuw@users.noreply.github.com>
2025-04-14 01:15:04 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/', // <-- Cấu hình này rất quan trọng khi deploy
+  plugins: [react(), tailwindcss()],
 })
